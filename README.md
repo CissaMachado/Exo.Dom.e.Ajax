57fa232a6247499c68c0014b0505170b79f98113
CissaMachado <ceciliadsmachado@gmail.com>
2022-10-17 00:26:05 -0300

    Versão Final Trabalho

--- README.md
+++ README.md
@@ -1,24 +1,3 @@
 # Exo.Dom.e.Ajax
  Exo feito na Disciplina de CpwII
 
- function gatosInfinitos(fotos = 18) {
-    let i = 0;
-    while (i < fotos) {
-        const recebe = document.getElementById('recebe');
-        var cats = Math.floor(Math.random() * 15);
-        const img = document.createElement('img');
-        img.src = imagens[cats];
-        recebe.appendChild(img);
-        i++;
-    }
-}
-gatosInfinitos();
-
-window.addEventListener('scroll', () => {
-    console.log("scrolled", window.scrollY)
-    console.log(window.innerHeight)
-    if (window.scrollY + window.innerHeight <= document.documentElement.scrollHeight) {
-
-        gatosInfinitos();
-    }
-})
--- script.js
+++ script.js
@@ -1,21 +1,43 @@
-
-function ajax(method, url) {
-  return new Promise(
-    (resolve, reject) => {
-      let xhr = new XMLHttpRequest();
-      xhr.open(method, url, true);
-
-      xhr.onreadystatechange = () => {
-        if (xhr.status == "200" && xhr.readyState == "4") {
-          resolve(xhr.responseText);
-        }
-      };
-      xhr.onerror = () => {
-        reject("ERRO")
-      }
-      xhr.send();
+function carregaImgs() {
+  return new Promise((resolve, reject) => {
+    let xml = new XMLHttpRequest()
+    xml.open("GET", "http://127.0.0.1:5500/data.json", true);
+    xml.setRequestHeader("content-type","aplication/json");
+    xml.onload = function (){
+      if (xml.status === 200) {
+        resolve(xml.response)
+    } else {
+        reject(Error)
     }
-  );
-
-
+    }
+    xml.onerror = function() {
+      reject(Error('Erro ferrou.'))
+  }
+  xml.send()
+  })
 }
+window.addEventListener('load', function() {
+  carregaImgs().then(function(response) {
+      let imgs = JSON.parse(response)
+  
+      function createImgElement(url) {
+          let img = document.createElement("img")
+          img.src = url
+          return img
+      }
+      
+      function insertImgElement(id) {
+          document.getElementById(id).appendChild(createImgElement(imgs[(Math.floor(Math.random() * 15))].url))
+      }
+      
+      for (let i = 0; i < 10; i++) {
+          insertImgElement("recebe")
+      }
+  
+      window.addEventListener("scroll", function() {
+          if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+              insertImgElement("recebe")
+          }
+      })
+  })
+})
